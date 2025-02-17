6340266f92a2b89f1ed51c48270d55b445fc7017
Fausto Carvalho <faustocarva@gmail.com>
2025-02-17 16:50:09 -0300

    add more validation to pydantic

--- script/genai4fuzz/model/testcase.py
+++ script/genai4fuzz/model/testcase.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Any, Union
+import re
 from pydantic import BaseModel, root_validator, ValidationError, conint, validator
 
 
@@ -11,6 +12,20 @@ class DeployTx(BaseModel):
     Params: Optional[list[Any]] = None
     Timestamp: str
     Blocknum: str
+    
+    @validator('From')
+    def validate_from_field(cls, v):
+        if not re.match(r'SmartianAgent\d+$', v):
+            raise ValueError('From field must be in format SmartianAgent followed by numbers')
+        return v
+
+    
+    
+    @validator('Timestamp', 'Blocknum')
+    def validate_numeric_fields(cls, v):
+        if not v.isdigit():  # This ensures string contains only digits
+            raise ValueError('Field must contain only numeric characters')
+        return v
 
 class Tx(BaseModel):
     From: str
@@ -22,8 +37,15 @@ class Tx(BaseModel):
 
     @validator('From')
     def validate_from_field(cls, v):
-        if not v.startswith('SmartianAgent'):
-            raise ValueError('From field must start with SmartianAgent')
+        if not re.match(r'SmartianAgent\d+$', v):
+            raise ValueError('From field must be in format SmartianAgent followed by numbers')
+        return v
+
+    
+    @validator('Timestamp', 'Blocknum')
+    def validate_numeric_fields(cls, v):
+        if not v.isdigit():  # This ensures string contains only digits
+            raise ValueError('Field must contain only numeric characters')
         return v
     
 class TestCaseModel(BaseModel):
